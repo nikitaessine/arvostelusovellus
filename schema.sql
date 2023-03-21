dfb7bf29e946e787a45e292865de202558c897d6
Nikita Essine <nikita.essine@helsinki.fi>
2023-03-21 16:03:02 +0200

    schema update

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
-    password TEXT
+    password TEXT,
+    admin BOOLEAN NOT NULL DEFAULT false
 );
 
 CREATE TABLE restaurants(
