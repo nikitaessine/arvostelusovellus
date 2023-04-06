c00cb066ac6c45f1f1df00aad63117ed30f716c6
Nikita Essine <nikita.essine@helsinki.fi>
2023-04-06 13:45:51 +0300

    gettin category from db

--- reviews.py
+++ reviews.py
@@ -2,12 +2,13 @@ from sqlalchemy.sql import text
 from db import db
 
 def get_reviews():
-    result = db.session.execute(text("SELECT restaurants.*, locations.address, locations.city,\
+    result = db.session.execute(text("SELECT restaurants.*, locations.address, locations.city, categories.category,\
                                     AVG(reviews.stars) AS avg_stars, COUNT(reviews.id) AS review_count,\
                                     array_agg(reviews.comment) AS comments FROM restaurants \
                                     LEFT JOIN locations ON restaurants.id = locations.restaurant_id \
                                     LEFT JOIN reviews ON restaurants.id = reviews.restaurant_id \
-                                    GROUP BY restaurants.id , locations.id"))
+                                    LEFT JOIN categories ON restaurants.id = categories.restaurant_id \
+                                    GROUP BY restaurants.id , locations.id, categories.id"))
     return result.fetchall()
 
 def add_review(restaurant_id, stars, comment):
