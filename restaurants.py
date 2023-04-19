b979bddcbe959f627960b74333c19e22422e0657
Nikita Essine <nikita.essine@helsinki.fi>
2023-04-19 11:49:53 +0300

    poiston yhteydessä esiintyvä virhe korjattu

--- restaurants.py
+++ restaurants.py
@@ -15,16 +15,20 @@ def search_restaurants(substring):
     return filtered_restaurants 
 
 def delete_restaurant(restaurant_id):
+    sql1 = text("DELETE FROM locations WHERE restaurant_id=:restaurant_id")
+    db.session.execute(sql1, {"restaurant_id": restaurant_id})
+    sql2 = text("DELETE FROM reviews WHERE restaurant_id=:restaurant_id")
+    db.session.execute(sql2, {"restaurant_id": restaurant_id})
 
-    sql2 = text("DELETE FROM locations WHERE restaurant_id=:restaurant_id")
-    db.session.execute(sql2, {"restaurant_id":restaurant_id})
-    sql3 = text("DELETE FROM reviews WHERE restaurant_id=:restaurant_id")
-    db.session.execute(sql3, {"restaurant_id":restaurant_id})
-    sql = text("DELETE FROM restaurants WHERE id=:restaurant_id")
-    db.session.execute(sql, {"restaurant_id":restaurant_id})
+
+    sql3 = text("DELETE FROM categories WHERE restaurant_id=:restaurant_id")
+    db.session.execute(sql3, {"restaurant_id": restaurant_id})
+
+
+    sql4 = text("DELETE FROM restaurants WHERE id=:restaurant_id")
+    db.session.execute(sql4, {"restaurant_id": restaurant_id})
 
     db.session.commit()
-
 
 def add_restaurant(name, address, city, categories):
     check_for_name = text("SELECT * FROM restaurants WHERE name = :name")
